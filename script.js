c2005dc31791459569743ce40285573cdbc58a86
mari2654 <mariana.santos2654@gmailcom>
2022-06-09 22:54:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+let nome="mariana";
+console.log(nome);
+
+let numero=null
+console.log(20);
+
+let numero1=2022;
+let numero2=20;
+console.log(numero1-numero2);
+
+console.log(estouMexendoNoConsole.log);
+
+const quartaFeira=true;
+console.log(type of quartaFeira);
